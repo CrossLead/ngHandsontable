dd003dd05e5658caa612da44ce81008e8e19f1f6
Ben Southgate <bsouthga@gmail.com>
2016-08-16 18:07:57 -0400

    no require alias

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ var _global = (
   (typeof global !== "undefined" && global)
 );
 
-var _require = (typeof require !== "function")
-  ? function() {};
-  : require;
+_global.Handsontable = _global.Handsontable || require('handsontable');
 
-_global.Handsontable = _global.Handsontable || _require('handsontable');
-_global.angular = _global.angular || _require('angular');
+require('./ie-shim');
+require('./ngHandsontable');
 
-_require('./ie-shim');
-_require('./ngHandsontable');
+require('./services/autoCompleteFactory');
+require('./services/hotRegisterer');
+require('./services/settingFactory');
 
-_require('./services/autoCompleteFactory');
-_require('./services/hotRegisterer');
-_require('./services/settingFactory');
-
-_require('./directives/hotAutocomplete');
-_require('./directives/hotColumn');
-_require('./directives/hotTable');
+require('./directives/hotAutocomplete');
+require('./directives/hotColumn');
+require('./directives/hotTable');
